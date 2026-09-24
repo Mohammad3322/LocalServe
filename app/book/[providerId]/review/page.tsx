aa4df2243696c7ac949d523b4062926125b3b21f
Mohammad <mohammad332243@gmail.com.com>
2026-09-24 20:07:52 +0300

    Updat  ReviewPage

--- app/book/[providerId]/review/page.tsx
+++ app/book/[providerId]/review/page.tsx
@@ -1,4 +1,4 @@
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 
 import { BookingReview } from "@/components/booking/BookingReview";
 import { getProviderById } from "@/lib/services/providers";
@@ -7,44 +7,16 @@ type ReviewPageProps = {
   params: Promise<{
     providerId: string;
   }>;
-  searchParams: Promise<{
-    service?: string;
-    date?: string;
-    time?: string;
-    customerName?: string;
-    customerEmail?: string;
-    customerPhone?: string;
-    notes?: string;
-  }>;
 };
 
-export default async function ReviewPage({
-  params,
-  searchParams,
-}: ReviewPageProps) {
+export default async function ReviewPage({ params }: ReviewPageProps) {
   const { providerId } = await params;
-
-  const { service, date, time, customerName, customerEmail } =
-    await searchParams;
 
   const provider = getProviderById(providerId);
 
   if (!provider) {
     notFound();
   }
-
-  // if (!service) {
-  //   console.log("!service");
-  //   redirect(`/book/${providerId}`);
-  // }
-  // if (!date || !time) {
-  //   console.log(" !date || !time ");
-  //   redirect(`/book/${providerId}`);
-  // }
-  // if (!customerName || !customerEmail) {
-  //   console.log("!customerName || !customerEmail");
-  //   redirect(`/book/${providerId}`);
-  // }
 
   return <BookingReview provider={provider} />;
 }
